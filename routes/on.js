85e785dd6b783e22b7dbdf983595632ee6f0e95a
Geison Camara <geison.camara@digitaldesk.com.br>
2016-06-30 16:43:39 -0300

    Correção de rotas

--- routes/on.js
+++ routes/on.js
@@ -2,7 +2,7 @@ var express = require('express');
 var rpi = require('./../raspberry/led.js');
 var router = express.Router();
 
-router.get('/on', function(req, res, next) {
+router.get('/', function(req, res, next) {
   rpi.changeLed(1);
   res.render('on', { title: 'Raspberry Pi Server' });
 });
